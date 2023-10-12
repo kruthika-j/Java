422bb2e9c6ab2a1bdedebdb5fe22c228770b8c4b
Kiruthika J <kiruthikajaga2003@gmail.com>
2023-10-12 21:50:44 +0530

    Collections

---
+++ Arraylist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Arraylist{
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<String>();
+        String[] fruits = {"apple","banana","grapes"};
+
+        for(int i=0;i<fruits.length;i++){
+            a.add(fruits[i]);
+        }
+
+        for(int j=0;j<a.size();j++){
+            System.out.println(a.get(j));
+        }
+
+    }
+}
---
+++ Linkedlist.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Linkedlist {
+    public static void main(String[] args) {
+        List<String> a = new LinkedList<String>();
+        String[] fruits = {"apple","banana","grapes"};
+
+        for(int i=0;i<fruits.length;i++){
+            a.add(fruits[i]);
+        }
+
+        for(int j=0;j<a.size();j++){
+            System.out.println(a.get(j));
+        }
+    }
+}
